8884344d4a8bbde5039e1effee6a3a9cb342aa6a
Ayush Thakur <113970894+ayushthakur04@users.noreply.github.com>
2023-01-06 22:21:39 +0530

    Create unordered map.cpp

---
+++ Hash/unordered map.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,number;
+    cin>>n;
+    string name;
+    unordered_map<string,int>marks;
+    for(int i=0;i<n;i++){
+        cin>>name>>number;
+        marks[name]=number;
+        
+    }
+    cout<<"Enter the name "<<endl;
+    cin>>name;
+    if(marks.find(name)==marks.end()){
+        cout<<"Not found"<<endl;
+    }
+    else{
+        cout<<" marks are "<<marks[name]<<endl;
+    }
+}
