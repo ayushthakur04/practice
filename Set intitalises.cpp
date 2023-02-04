4a8d2440beadb636d306b61d8ea1d3bc76e92f00
Ayush Thakur <113970894+ayushthakur04@users.noreply.github.com>
2023-02-04 23:15:23 +0530

    Create Set intitalises.cpp

---
+++ Set intitalises.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	set<int>s;
+	s.insert(1);
+	s.insert(2);
+	
+		
+	
+	
+//	s.insert(1);
+//	s.insert(2);
+//	s.insert(9);
+	for(int a:s){
+		cout<<" elements are : "<<a<<endl;
+	}
+	return 0;
+}
